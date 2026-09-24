8baa2b3e7b3e9ca7f32119d3f5adf5ad9ec8b339
binyoun-cberi <binyoun@cberi.go.kr>
2026-09-24 14:04:59 +0900

    Register new game storage keys

--- kidscade-storage.js
+++ kidscade-storage.js
@@ -62,7 +62,9 @@
     gugudanWins: 'kidscade_gugudan_wins_v1',
     gugudanHighScore: 'kidscade_gugudan_high_score_v1',
     topKingRecord: 'kidscade_top_king_record_v1',
-    metroPlannerSave: 'kidscade_metro_planner_v1'
+    metroPlannerSave: 'kidscade_metro_planner_v1',
+    disasterCityBestSeconds: 'kidscade_disaster_city_best_seconds',
+    folkloreNightSave: 'kidscade_folklore_night_v1'
   });
 
   // Dynamic namespaces are prefixes, not concrete localStorage records.
